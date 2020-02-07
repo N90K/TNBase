049c11f606d05f3cfc35f0fc784d12125a4f8003
Audrius Asadauskas <audriusa83@gmail.com>
2020-02-07 18:18:27 +0000

    Updated version number

--- TNBase/Properties/AssemblyInfo.cs
+++ TNBase/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.6.5.2")]
-[assembly: AssemblyFileVersion("1.6.5.2")]
+[assembly: AssemblyVersion("1.6.5.3")]
+[assembly: AssemblyFileVersion("1.6.5.3")]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
