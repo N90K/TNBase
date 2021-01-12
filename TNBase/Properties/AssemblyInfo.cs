72e1fff7754d9bcfb90de82ab7247c3f3b54dc30
Audrius <audriusa83@gmail.com>
2021-01-12 12:39:32 +0000

    Update AssemblyInfo.cs

--- TNBase/Properties/AssemblyInfo.cs
+++ TNBase/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.6.5.4")]
-[assembly: AssemblyFileVersion("1.6.5.4")]
+[assembly: AssemblyVersion("1.6.5.5")]
+[assembly: AssemblyFileVersion("1.6.5.5")]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
