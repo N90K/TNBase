2203a94040a59b38a168a9a411dedd3be282d7a7
Audrius <audriusa83@gmail.com>
2021-07-11 21:17:41 +0100

    Closes #8 Print stack labels one postcode per page (#12)

--- TNBase/Forms/Printing/FormPrintGPOLabels.cs
+++ TNBase/Forms/Printing/FormPrintGPOLabels.cs
@@ -1,81 +1,72 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
+
 namespace TNBase
 {
     public partial class FormPrintGPOLabels
-	{
-        bool firstPage = true;
+    {
+        List<string> postcodes;
 
-		private void printLabels()
-		{
-			printPreview.Document = printGPODoc;
-			printPreview.ClientSize = new Size(600, 600);
-			printPreview.ShowDialog();
-		}
+        private void printLabels()
+        {
+            printPreview.Document = printGPODoc;
+            printPreview.ClientSize = new Size(600, 600);
+            printPreview.ShowDialog();
+        }
 
-		private void SetInitial()
-		{
-			firstPage = true;
-		}
+        private void SetInitial()
+        {
+            postcodes = new List<string> { "B90", "B91+B92", "B93" };
+        }
 
-		private void formPrintGPOLabels_Load(object sender, EventArgs e)
-		{
-			SetInitial();
-			printLabels();
-			this.Close();
-		}
+        private void formPrintGPOLabels_Load(object sender, EventArgs e)
+        {
+            SetInitial();
+            printLabels();
+            Close();
+        }
 
-		private void printGPODoc_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
-		{
-			Font reportFont = new System.Drawing.Font("Times New Roman", 24, FontStyle.Bold);
-			Font reportFontSmall = new System.Drawing.Font("Times New Roman", 12);
-			Font reportFontSmallBold = new System.Drawing.Font("Times New Roman", 14, FontStyle.Bold);
-			Font reportFontSmallBoldTitles = new System.Drawing.Font("Times New Roman", 16, FontStyle.Bold);
-			Font reportFontBigBoldTitles = new System.Drawing.Font("Times New Roman", 30, FontStyle.Bold);
-			Graphics g = e.Graphics;
-			int pageHeight = e.MarginBounds.Height;
+        private void printGPODoc_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font reportFontSmall = new Font("Times New Roman", 12);
+            Font reportFontBigBoldTitles = new Font("Times New Roman", 30, FontStyle.Bold);
+            Graphics g = e.Graphics;
+            var postcode = postcodes.First();
 
-			if (firstPage) {
-				e.HasMorePages = true;
-			}
+            if (postcodes.Count > 1)
+            {
+                e.HasMorePages = true;
+            }
 
-			for (int value = 0; value <= 18-1; value++) {
+            for (int value = 0; value <= 18 - 1; value++)
+            {
                 int myRow = ((int)Math.Ceiling((double)((double)(value + 1) / (double)3))) - 1;
                 int myColumn = (value % 3);
-                var theNumber = value+1;
-				if (!firstPage) {
-					theNumber += (18);
-				}
 
-				var initialY = (175 * myRow) + 50;
-				var initialX = (myColumn * 240) + 60;
+                var initialY = (175 * myRow) + 50;
+                var initialX = (myColumn * 240) + 60;
 
-				string myBoldText = "";
-				if ((theNumber <= 9)) {
-					myBoldText = "B90";
-				} else if ((theNumber <= (18 + 9))) {
-					myBoldText = "B91+B92";
-				} else {
-					myBoldText = "B93";
-				}
+                g.DrawString("For the attention of the" + Environment.NewLine + " NIGHT MANAGER", reportFontSmall, Brushes.Black, initialX, initialY);
+                g.DrawString(postcode, reportFontBigBoldTitles, Brushes.Black, initialX, initialY + 40);
+            }
 
-				g.DrawString("For the attention of the" + Environment.NewLine + " NIGHT MANAGER", reportFontSmall, Brushes.Black, initialX, initialY);
-				g.DrawString(myBoldText, reportFontBigBoldTitles, Brushes.Black, initialX, initialY + 40);
-			}
-			firstPage = false;
+            postcodes.Remove(postcode);
 
-			if (!e.HasMorePages) {
-				SetInitial();
-			}
-		}
-		public FormPrintGPOLabels()
-		{
-			InitializeComponent();
-		}
+            if (!e.HasMorePages)
+            {
+                SetInitial();
+            }
+        }
+        public FormPrintGPOLabels()
+        {
+            InitializeComponent();
+        }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
-	}
+    }
 }
