6b8019ea15c8d253b7acc768bd8d305cacb272c8
Audrius <audriusa83@gmail.com>
2022-03-23 00:05:46 +0000

    Closes #51 Adjustments to address label printing (#74)
    * Adjust address label
    
    * Update FormPrintAllLabels.cs
    
    * Update Release Notes.txt

--- src/TNBase/Forms/Printing/FormPrintAllLabels.cs
+++ src/TNBase/Forms/Printing/FormPrintAllLabels.cs
@@ -10,106 +10,122 @@ using Microsoft.Extensions.DependencyInjection;
 namespace TNBase
 {
     public partial class FormPrintAllLabels
-	{
-		private readonly IServiceLayer serviceLayer = Program.ServiceProvider.GetRequiredService<IServiceLayer>();
-		List<Listener> theListeners = new List<Listener>();
-		int totalCount = 0;
+    {
+        private readonly IServiceLayer serviceLayer = Program.ServiceProvider.GetRequiredService<IServiceLayer>();
+        List<Listener> theListeners = new List<Listener>();
+        int totalCount = 0;
 
-		int currentPageNumber = 0;
-		private void btnCancel_Click(object sender, EventArgs e)
-		{
-			this.Close();
-		}
+        int currentPageNumber = 0;
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
 
-		private void SetInitial()
-		{
-			// Get 4 lots of listeners (one for each label).
-			// Must use lists here to achieve the sorting (hence the casts).
-			theListeners = new List<Listener>();
-            theListeners.AddRange(serviceLayer.GetAlphabeticList().Cast<Listener>().ToList());
-			theListeners.AddRange(serviceLayer.GetAlphabeticList().Cast<Listener>().ToList());
+        private void SetInitial()
+        {
+            // Get 4 lots of listeners (one for each label).
+            // Must use lists here to achieve the sorting (hence the casts).
+            theListeners = new List<Listener>();
             theListeners.AddRange(serviceLayer.GetAlphabeticList().Cast<Listener>().ToList());
             theListeners.AddRange(serviceLayer.GetAlphabeticList().Cast<Listener>().ToList());
+            theListeners.AddRange(serviceLayer.GetAlphabeticList().Cast<Listener>().ToList());
+            theListeners.AddRange(serviceLayer.GetAlphabeticList().Cast<Listener>().ToList());
 
-			// Sort them.
-			theListeners.Sort(new IListener());
+            // Sort them.
+            theListeners.Sort(new IListener());
 
-			totalCount = theListeners.Count();
-			currentPageNumber = 0;
-		}
+            totalCount = theListeners.Count();
+            currentPageNumber = 0;
+        }
 
-		// Print labels.
-		private void printLabels()
-		{
-			SetInitial();
+        // Print labels.
+        private void printLabels()
+        {
+            SetInitial();
 
-			printPreview.Document = printLabelDoc;
-			printPreview.ClientSize = new Size(600, 600);
-			printPreview.ShowDialog();
-		}
+            printPreview.Document = printLabelDoc;
+            printPreview.ClientSize = new Size(600, 600);
+            printPreview.ShowDialog();
+        }
 
-		private void printLabelDoc_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
-		{
-			Font reportFont = new System.Drawing.Font("Times New Roman", 24, FontStyle.Bold);
-			Font reportFontSmall = new System.Drawing.Font("Times New Roman", 12);
-			Font reportFontSmallBold = new System.Drawing.Font("Times New Roman", 14, FontStyle.Bold);
-			Font reportFontSmallBoldTitles = new System.Drawing.Font("Times New Roman", 16, FontStyle.Bold);
-			Font reportFontBigBoldTitles = new System.Drawing.Font("Times New Roman", 20, FontStyle.Bold);
+        private void printLabelDoc_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font reportFont = new System.Drawing.Font("Times New Roman", 24, FontStyle.Bold);
+            Font reportFontSmall = new System.Drawing.Font("Times New Roman", 12);
+            Font reportFontSmallBold = new System.Drawing.Font("Times New Roman", 14, FontStyle.Bold);
+            Font reportFontSmallBoldTitles = new System.Drawing.Font("Times New Roman", 16, FontStyle.Bold);
+            Font reportFontBigBoldTitles = new System.Drawing.Font("Times New Roman", 20, FontStyle.Bold);
 
-			Graphics g = e.Graphics;
-			int pageHeight = e.MarginBounds.Height;
+            Graphics g = e.Graphics;
+            var lineHeight = 16;
+            var spacing = 4;
 
-			if (theListeners.Count >= 5) {
-				e.HasMorePages = true;
-			}
-			currentPageNumber = currentPageNumber + 1;
+            if (theListeners.Count >= 5)
+            {
+                e.HasMorePages = true;
+            }
+            currentPageNumber = currentPageNumber + 1;
 
-			int theIndex = 0;
+            int theIndex = 0;
 
-			int min = Math.Min(theListeners.Count, 3 * 6);
-			min = min - 1;
+            int min = Math.Min(theListeners.Count, 3 * 6);
+            min = min - 1;
 
-			for (int value = 0; value <= min; value++) {
-				int myRow = (int) Math.Ceiling((double) ((double)(value + 1) / (double) 3)) - 1;
-				int myColumn = (value % 3);
-				Listener myListener = theListeners[0];
+            for (int value = 0; value <= min; value++)
+            {
+                int myRow = (int)Math.Ceiling((double)((double)(value + 1) / (double)3)) - 1;
+                int myColumn = (value % 3);
+                Listener myListener = theListeners[0];
 
-				var initialY = (175 * myRow) + 50;
-				var initialX = (myColumn * 240) + Properties.Settings.Default.LabelXAdjust;
-				theIndex = theIndex + 1;
+                var initialY = (175 * myRow) + 50;
+                var initialX = (myColumn * 240) + Properties.Settings.Default.LabelXAdjust;
+                theIndex = theIndex + 1;
 
-				g.DrawString("First Class Post", reportFontSmallBoldTitles, Brushes.Black, initialX, initialY);
-				g.DrawString(myListener.Title + " " + myListener.Forename + " " + myListener.Surname, reportFontSmall, Brushes.Black, initialX, initialY + 16 + 4);
-				g.DrawString(myListener.Addr1, reportFontSmall, Brushes.Black, initialX, initialY + 32 + 4);
-				g.DrawString(myListener.Addr2, reportFontSmall, Brushes.Black, initialX, initialY + 48 + 4);
-				g.DrawString(myListener.Town + ", " + myListener.Postcode, reportFontSmall, Brushes.Black, initialX, initialY + 66 + 4);
+                var addressLines = new List<string>
+                {
+                    string.Join(" ", myListener.Title, myListener.Forename, myListener.Surname),
+                    myListener.Addr1,
+                    myListener.Addr2,
+                    myListener.Town,
+                    myListener.County,
+                    myListener.Postcode
+                }.Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
 
-				BarcodeLib.Barcode b = new BarcodeLib.Barcode();
-                Image newImage = b.Encode(BarcodeLib.TYPE.CODE39, myListener.Wallet.ToString(), Color.Black, Color.White, 150, 70);
 
-                e.Graphics.DrawImage(newImage, initialX - 10, initialY + 82 + 8, 150, 70);
+                g.DrawString("First Class Post", reportFontSmallBoldTitles, Brushes.Black, initialX, initialY);
 
-                g.DrawString(myListener.Wallet.ToString(), reportFontBigBoldTitles, Brushes.Black, initialX + 122, initialY + 82 + 4 + 8);
+                for (int i = 0; i < addressLines.Count; i++)
+                {
+                    g.DrawString(addressLines[i], reportFontSmall, Brushes.Black, initialX, initialY + lineHeight * (i + 1) + spacing);
+                }
 
-				theListeners.RemoveAt(0);
-			}
+                BarcodeLib.Barcode b = new BarcodeLib.Barcode();
+                Image newImage = b.Encode(BarcodeLib.TYPE.CODE39, myListener.Wallet.ToString(), Color.Black, Color.White, 150, 34);
 
-			// VB is stupid.... have to reset this so its back when you actually print it!
-			if (!(e.HasMorePages)) {
-				SetInitial();
-			}
-		}
+                e.Graphics.DrawImage(newImage, initialX - 10, initialY + lineHeight * 7 + spacing * 2, 150, 34);
 
-		private void formPrintAllLabels_Load(object sender, EventArgs e)
-		{
+                g.DrawString(myListener.Wallet.ToString(), reportFontBigBoldTitles, Brushes.Black, initialX + 122, initialY + lineHeight * 7 + spacing * 3);
+
+                theListeners.RemoveAt(0);
+            }
+
+            // VB is stupid.... have to reset this so its back when you actually print it!
+            if (!(e.HasMorePages))
+            {
+                SetInitial();
+            }
+        }
+
+        private void formPrintAllLabels_Load(object sender, EventArgs e)
+        {
             MessageBox.Show("Ensure you insert labels into the printer tray before printing.", ModuleGeneric.getAppShortName(), MessageBoxButtons.OK);
-			printLabels();
+            printLabels();
             MessageBox.Show("Make sure you replace the labels in the printer tray with plain paper.", ModuleGeneric.getAppShortName(), MessageBoxButtons.OK);
-			this.Close();
-		}
-		public FormPrintAllLabels()
-		{
-			InitializeComponent();
-		}
-	}
+            this.Close();
+        }
+        public FormPrintAllLabels()
+        {
+            InitializeComponent();
+        }
+    }
 }
--- src/TNBase/Forms/Printing/FormPrintLabels.cs
+++ src/TNBase/Forms/Printing/FormPrintLabels.cs
@@ -1,77 +1,92 @@
 using Microsoft.VisualBasic;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using TNBase.Objects;
 
 namespace TNBase
 {
     public partial class FormPrintLabels
-	{
-		Listener myListener;
+    {
+        Listener myListener;
 
-		int myIndex = 0;
-		public void setupForm(Listener theListener, int startIndex)
-		{
-			myListener = theListener;
-			myIndex = startIndex;
-			//MsgBox("Ensure you insert labels into the printer tray before printing.", MessageBoxButtons.OkOnly)
-			Interaction.MsgBox("Please choose the correct printer for label printing by right clicking on the printer icon on the next screen - top left picture of printer.");
-			printLabels();
-			//MsgBox("Make sure you replace the labels in the printer tray with plain paper.", MessageBoxButtons.OkOnly)
-			this.Close();
-		}
+        int myIndex = 0;
+        public void setupForm(Listener theListener, int startIndex)
+        {
+            myListener = theListener;
+            myIndex = startIndex;
+            //MsgBox("Ensure you insert labels into the printer tray before printing.", MessageBoxButtons.OkOnly)
+            Interaction.MsgBox("Please choose the correct printer for label printing by right clicking on the printer icon on the next screen - top left picture of printer.");
+            printLabels();
+            //MsgBox("Make sure you replace the labels in the printer tray with plain paper.", MessageBoxButtons.OkOnly)
+            this.Close();
+        }
 
-		private void btnCancel_Click(object sender, EventArgs e)
-		{
-			this.Close();
-		}
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
 
-		// Print labels.
-		private void printLabels()
-		{
-			printPreview.Document = printLabelDoc;
-			printPreview.ClientSize = new Size(600, 600);
-			printPreview.ShowDialog();
-		}
+        // Print labels.
+        private void printLabels()
+        {
+            printPreview.Document = printLabelDoc;
+            printPreview.ClientSize = new Size(600, 600);
+            printPreview.ShowDialog();
+        }
 
-		private void printLabelDoc_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
-		{
-			Font reportFont = new System.Drawing.Font("Times New Roman", 24, FontStyle.Bold);
-			Font reportFontSmall = new System.Drawing.Font("Times New Roman", 12);
-			Font reportFontSmallBold = new System.Drawing.Font("Times New Roman", 14, FontStyle.Bold);
-			Font reportFontSmallBoldTitles = new System.Drawing.Font("Times New Roman", 16, FontStyle.Bold);
-			Font reportFontBigBoldTitles = new System.Drawing.Font("Times New Roman", 20, FontStyle.Bold);
+        private void printLabelDoc_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font reportFont = new System.Drawing.Font("Times New Roman", 24, FontStyle.Bold);
+            Font reportFontSmall = new System.Drawing.Font("Times New Roman", 12);
+            Font reportFontSmallBold = new System.Drawing.Font("Times New Roman", 14, FontStyle.Bold);
+            Font reportFontSmallBoldTitles = new System.Drawing.Font("Times New Roman", 16, FontStyle.Bold);
+            Font reportFontBigBoldTitles = new System.Drawing.Font("Times New Roman", 20, FontStyle.Bold);
 
-			Graphics g = e.Graphics;
-			int pageHeight = e.MarginBounds.Height;
+            Graphics g = e.Graphics;
+            var lineHeight = 16;
+            var spacing = 4;
 
-			int theIndex = myIndex;
+            int theIndex = myIndex;
 
-			for (int value = 0; value <= 3; value++) {
-				int myRow = (int) Math.Ceiling((double) ((double)(theIndex + 1) / (double)3)) - 1;
-				int myColumn = (theIndex % 3);
+            var addressLines = new List<string>
+            {
+                string.Join(" ", myListener.Title, myListener.Forename, myListener.Surname),
+                myListener.Addr1,
+                myListener.Addr2,
+                myListener.Town, 
+                myListener.County,
+                myListener.Postcode
+            }.Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
 
-				var initialY = (175 * myRow) + 34;
-				var initialX = (myColumn * 240) + Properties.Settings.Default.LabelXAdjust;
-				theIndex = theIndex + 1;
+            for (int value = 0; value <= 3; value++)
+            {
+                int myRow = (int)Math.Ceiling((double)((double)(theIndex + 1) / (double)3)) - 1;
+                int myColumn = (theIndex % 3);
 
-				g.DrawString("First Class Post", reportFontSmallBoldTitles, Brushes.Black, initialX, initialY);
-				g.DrawString(myListener.Title + " " + myListener.Forename + " " + myListener.Surname, reportFontSmall, Brushes.Black, initialX, initialY + 16 + 4);
-				g.DrawString(myListener.Addr1, reportFontSmall, Brushes.Black, initialX, initialY + 32 + 4);
-				g.DrawString(myListener.Addr2, reportFontSmall, Brushes.Black, initialX, initialY + 48 + 4);
-				g.DrawString(myListener.Town + ", " + myListener.Postcode, reportFontSmall, Brushes.Black, initialX, initialY + 66 + 4);
-                
+                var initialY = (175 * myRow) + 40;
+                var initialX = (myColumn * 240) + Properties.Settings.Default.LabelXAdjust;
+                theIndex++;
+
+                g.DrawString("First Class Post", reportFontSmallBoldTitles, Brushes.Black, initialX, initialY);
+
+                for (int i = 0; i < addressLines.Count; i++)
+                {
+                    g.DrawString(addressLines[i], reportFontSmall, Brushes.Black, initialX, initialY + lineHeight * (i + 1) + spacing);
+                }
+
                 BarcodeLib.Barcode b = new BarcodeLib.Barcode();
-                Image newImage = b.Encode(BarcodeLib.TYPE.CODE39, myListener.Wallet.ToString(), Color.Black, Color.White, 150, 70);
+                Image newImage = b.Encode(BarcodeLib.TYPE.CODE39, myListener.Wallet.ToString(), Color.Black, Color.White, 150, 34);
 
-                e.Graphics.DrawImage(newImage, initialX - 10, initialY + 82 + 8, 150, 70);
+                e.Graphics.DrawImage(newImage, initialX - 10, initialY + lineHeight * 7 + spacing * 2, 150, 34);
 
-                g.DrawString(myListener.Wallet.ToString(), reportFontBigBoldTitles, Brushes.Black, initialX + 122, initialY + 82 + 4 + 8);
-			}
-		}
-		public FormPrintLabels()
-		{
-			InitializeComponent();
-		}
-	}
+                g.DrawString(myListener.Wallet.ToString(), reportFontBigBoldTitles, Brushes.Black, initialX + 122, initialY + lineHeight * 7 + spacing * 3);
+            }
+        }
+        public FormPrintLabels()
+        {
+            InitializeComponent();
+        }
+    }
 }
