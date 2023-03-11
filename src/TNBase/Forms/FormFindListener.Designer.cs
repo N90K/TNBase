a5ca0cc2c46f84142a16eff064ba7bff2c963c8f
Audrius <audriusa83@gmail.com>
2023-03-11 22:00:39 +0000

    Fixes #104 Find Listener form layout is broken (#105)

--- src/TNBase/Forms/FormFindListener.Designer.cs
+++ src/TNBase/Forms/FormFindListener.Designer.cs
@@ -52,8 +52,8 @@ namespace TNBase
             // btnSearch
             // 
             this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearch.Location = new System.Drawing.Point(550, 373);
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSearch.Location = new System.Drawing.Point(689, 475);
             this.btnSearch.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(316, 80);
@@ -65,83 +65,83 @@ namespace TNBase
             // Label2
             // 
             this.Label2.AutoSize = true;
-            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label2.Location = new System.Drawing.Point(252, 23);
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Label2.Location = new System.Drawing.Point(142, 9);
             this.Label2.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.Label2.Name = "Label2";
-            this.Label2.Size = new System.Drawing.Size(488, 66);
+            this.Label2.Size = new System.Drawing.Size(787, 110);
             this.Label2.TabIndex = 2;
             this.Label2.Text = "Type in a Forename and Surname\r\n         or a Wallet Number";
             // 
             // Label3
             // 
             this.Label3.AutoSize = true;
-            this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label3.Location = new System.Drawing.Point(18, 117);
+            this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label3.Location = new System.Drawing.Point(18, 148);
             this.Label3.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.Label3.Name = "Label3";
-            this.Label3.Size = new System.Drawing.Size(230, 33);
+            this.Label3.Size = new System.Drawing.Size(354, 55);
             this.Label3.TabIndex = 4;
             this.Label3.Text = "Wallet Number:";
             // 
             // txtWallet
             // 
-            this.txtWallet.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtWallet.Location = new System.Drawing.Point(24, 158);
+            this.txtWallet.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtWallet.Location = new System.Drawing.Point(18, 211);
             this.txtWallet.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
             this.txtWallet.MaxLength = 50;
             this.txtWallet.Name = "txtWallet";
-            this.txtWallet.Size = new System.Drawing.Size(891, 40);
+            this.txtWallet.Size = new System.Drawing.Size(429, 62);
             this.txtWallet.TabIndex = 1;
             this.txtWallet.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtWallet_KeyPress);
             // 
             // Label1
             // 
             this.Label1.AutoSize = true;
-            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label1.Location = new System.Drawing.Point(18, 245);
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label1.Location = new System.Drawing.Point(24, 295);
             this.Label1.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(165, 33);
+            this.Label1.Size = new System.Drawing.Size(257, 55);
             this.Label1.TabIndex = 6;
             this.Label1.Text = "Forename:";
             // 
             // txtForename
             // 
-            this.txtForename.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtForename.Location = new System.Drawing.Point(24, 286);
+            this.txtForename.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtForename.Location = new System.Drawing.Point(24, 358);
             this.txtForename.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
             this.txtForename.MaxLength = 50;
             this.txtForename.Name = "txtForename";
-            this.txtForename.Size = new System.Drawing.Size(435, 40);
+            this.txtForename.Size = new System.Drawing.Size(486, 62);
             this.txtForename.TabIndex = 2;
             // 
             // txtSurname
             // 
-            this.txtSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSurname.Location = new System.Drawing.Point(477, 286);
+            this.txtSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtSurname.Location = new System.Drawing.Point(562, 358);
             this.txtSurname.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
             this.txtSurname.MaxLength = 50;
             this.txtSurname.Name = "txtSurname";
-            this.txtSurname.Size = new System.Drawing.Size(438, 40);
+            this.txtSurname.Size = new System.Drawing.Size(477, 62);
             this.txtSurname.TabIndex = 3;
             // 
             // Label4
             // 
             this.Label4.AutoSize = true;
-            this.Label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label4.Location = new System.Drawing.Point(471, 245);
+            this.Label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label4.Location = new System.Drawing.Point(562, 295);
             this.Label4.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.Label4.Name = "Label4";
-            this.Label4.Size = new System.Drawing.Size(149, 33);
+            this.Label4.Size = new System.Drawing.Size(233, 55);
             this.Label4.TabIndex = 9;
             this.Label4.Text = "Surname:";
             // 
             // btnCancel
             // 
             this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(68, 373);
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCancel.Location = new System.Drawing.Point(56, 475);
             this.btnCancel.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(316, 80);
@@ -150,12 +150,12 @@ namespace TNBase
             this.btnCancel.UseVisualStyleBackColor = false;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // formFindListener
+            // FormFindListener
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 33F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(35F, 67F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(939, 500);
+            this.ClientSize = new System.Drawing.Size(1076, 599);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.Label4);
             this.Controls.Add(this.txtSurname);
@@ -165,13 +165,13 @@ namespace TNBase
             this.Controls.Add(this.Label3);
             this.Controls.Add(this.Label2);
             this.Controls.Add(this.btnSearch);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "formFindListener";
+            this.Name = "FormFindListener";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
             this.PerformLayout();
